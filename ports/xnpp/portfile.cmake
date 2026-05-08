29e70c2f22b66c5cfcaca82631d9dca5fdc9a211
Danilo Borges Santos <borges_santos89@hotmail.com>
2026-05-08 14:44:44 -0300

    Adiciona xnpp-content-cli ao portile.cmake

--- ports/xnpp/portfile.cmake
+++ ports/xnpp/portfile.cmake
@@ -1,19 +1,33 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO borgesdan/xnpp
-    REF v0.5.1
-    SHA512 e210ef8e18bf77a032229703aabdb95262335d2d734f8e579300c0682f84c2ea17d4fb577249a3f30c77990e10a1ce1927e9dd138daa3523e2323d901ada062c
+    REF v0.5.8
+    SHA512 0ce4997ff6e52856902d75567f0f6a8bb0d419b2e39d7cbe32530d70d728082667c90c79b7bcf06679b0c454df970bba4e39135ba2e6fbf7914d82831aeced28
 )
+
+set(BUILD_CONTENTPIPELINE OFF)
+
+if("content-pipeline" IN_LIST FEATURES)
+    set(BUILD_CONTENTPIPELINE ON)
+endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
         -DXNPP_BUILD_LIB=ON
-        -DXNPP_BUILD_CONTENTPIPELINE=OFF
-        -DXNPP_BUILD_GAME_TEST=OFF
+        -DXNPP_BUILD_CONTENT_PIPELINE=${BUILD_CONTENTPIPELINE}
+        -DXNPP_BUILD_CONTENT_CLI=${BUILD_CONTENTPIPELINE}
+        -DXNPP_BUILD_GAME_TEST=OFF        
 )
 
 vcpkg_cmake_install()
+
+if(BUILD_CONTENTPIPELINE)
+    vcpkg_copy_tools(
+        TOOL_NAMES xnpp-content-cli
+        AUTO_CLEAN
+    )
+endif()
 
 vcpkg_cmake_config_fixup(
     CONFIG_PATH lib/cmake/xnpp
@@ -23,4 +37,7 @@ vcpkg_install_copyright(
     FILE_LIST "${SOURCE_PATH}/LICENSE.md"
 )
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE 
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+)
